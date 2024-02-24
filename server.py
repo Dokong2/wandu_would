c1c5d7e242bc355d187c6bac2e07d342299dbd26
Dokong2 <jiwan6942@gmail.com>
2024-02-24 20:26:25 +0900

    땅콩마시따

--- server.py
+++ server.py
@@ -2,16 +2,31 @@ import threading
 import pygame
 import winsound
 import time
+import socket
 # made by dokong2
 # 이 파이썬 프로젝트는 비영리 목적이며 테스트로 만들어보는 사이드프로젝트입니다.
 # 깃허브 : https://github.com/Dokong2/wandu_would
 
 
-def log(msg):
-    now = time
-    print(now.localtime().tm_hour + " : " + now.localtime().tm_min + " : " + now.localtime().tm_sec + " " + msg)
-
+class log():
+    def write(msg):
+        now = time
+        print(str(now.localtime().tm_hour) + "시" + str(now.localtime().tm_min) + "분" + str(now.localtime().tm_sec) + "초" + " : " + msg)
 
 def startwindow():
-    print("이 콘솔에 출력되는 메시지는 테스트 용이며 실제 exe 확장자에선 콘솔이 실행되지 않을 것입니다.")
+    log.write("서버 생성을 시작합니다.")
+    log.write("x좌표 생성..")
+    host = socket.gethostname()
+    x = socket.socket()
+    x.bind((host, 1010))
+    log.write("y좌표 생성..")
+    y = socket.socket()
+    y.bind((host, 2020))
+    log.write("이모지 서버 생성..")
+    emoge = socket.socket()
+    emoge.bind((host, 8080))
+    log.write("모든 서버가 생성됐습니다.")
     
+    
+if __name__ == "__main__":
+    startwindow()
