c217c752619eac2d863feafc9bc1d4b754038a23
Dokong2 <jiwan6942@gmail.com>
2024-02-24 18:28:38 +0900

    아 방향을 잡아보자..

--- start.py
+++ start.py
@@ -1,8 +1,9 @@
 import threading
 import pygame
 import winsound
+kill = 1
 # made by dokong2
 # 이 파이썬 프로젝트는 비영리 목적이며 테스트로 만들어보는 사이드프로젝트입니다.
-# 깃허브 : 
+# 깃허브 : https://github.com/Dokong2/wandu_would
 
 def startwindow():
