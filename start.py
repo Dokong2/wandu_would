9b108670afe3a550701efbfecb4eb3a944f02174
Dokong2 <jiwan6942@gmail.com>
2024-02-24 18:30:46 +0900

    엔트리 못참아

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 import threading
 import pygame
 import winsound
-kill = 1
 # made by dokong2
 # 이 파이썬 프로젝트는 비영리 목적이며 테스트로 만들어보는 사이드프로젝트입니다.
 # 깃허브 : https://github.com/Dokong2/wandu_would
 
 def startwindow():
+    print("이 콘솔에 출력되는 메시지는 테스트 용이며 실제 exe 확장자에선 콘솔이 실행되지 않을 것입니다.")
